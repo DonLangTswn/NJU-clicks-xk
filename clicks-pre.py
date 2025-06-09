a23a9036aa6da104e2ddab666548fde9b535e7a8
DonLangTswn <donlangtswn@outlook.com>
2025-06-09 21:05:48 +0800

    ver2.0: 支持自动更新webdriver; 加入了异常监测机制; 自动下载依赖

--- clicks-pre.py
+++ clicks-pre.py
@@ -1,13 +1,31 @@
 """
     Run when you first use clicks with potatos.
+    Firstly check your python dependencies.
+
+    Last updated: 2025.6.9
 """
+import sys
+import subprocess
+
+# check dependencies
+def ensure_package(pkg):
+    try:
+        __import__(pkg)
+    except ImportError:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', pkg])
+
+ensure_package('selenium')
+ensure_package('colorama')
+ensure_package('filelock')
+ensure_package('webdriver_manager')
+
 import time
 import clicks_xk as click
 import myLog as log
 
 url = 'https://chromewebstore.google.com/search/PotatoPlus?utm_source=ext_app_menu'
 rectangle = '//*[@id="yDmH0d"]/c-wiz/div/div/div/main/section/div[1]/div/a'
-addChrome = '//*[@id="yDmH0d"]/c-wiz[2]/div/div/main/div/section[1]/section/div[2]/div/button/span[6]'
+addChrome = '//*[@id="yDmH0d"]/c-wiz/div/div/main/div/section[1]/section/div/div[1]/div[2]/div/button/span[6]'
 
 try:
     driver = click.init_driver(potato=True)
@@ -18,9 +36,17 @@ try:
     time.sleep(1.2)
     log.INFO('Clicking to add to your Chrome...')
     click.try_to_click(driver, addChrome, url)
-    log.DONE('Please click to confirm the addition.')
+    log.DONE('Please click to confirm the addition...')
+    time.sleep(3)
+    log.DONE('Use ctrl+C to finish.')
     time.sleep(3000)
+
+except click.ClickException:
+    log.FAIL('Failed to click to download PotatoPlus, check your connection first.')
+    driver.quit()
+    exit()
 
 except KeyboardInterrupt:
     log.DONE('Exited.')
+    driver.quit()
     exit()
